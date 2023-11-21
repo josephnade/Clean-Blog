219a0ab733b512dabdf52ed85fea37033bc58d29
josephnade <yusuf.akn2017@hotmail.com>
2023-11-21 21:52:14 +0300

    :rocket:

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
-
 const app = express();
 
 const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description",
-    };
-    res.send(blog);
+    res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/add_post", (req, res) => {
+    res.render("add_post.ejs");
+});
+
+app.get("/post", (req, res) => {
+    res.render("post.ejs");
 });
 
 app.listen(port, "localhost", () => {
